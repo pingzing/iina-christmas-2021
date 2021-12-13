c26c33fff16c7fc27c8bd6aee4c15deb13688d1f
pingzing <mcalistern@gmail.com>
2021-12-14 00:56:33 +0200

    Bump versions

--- IinaChristmas.Core/App.xaml.cs
+++ IinaChristmas.Core/App.xaml.cs
@@ -9,7 +9,7 @@ namespace IinaChristmas.Core
 {
     public partial class App : Application
     {
-        private static readonly Version CURRENT_VERSION = new Version(1, 0, 0);
+        private static readonly Version CURRENT_VERSION = new Version(1, 1, 0);
 
         public NavigationPage Navigation = new NavigationPage();
 
